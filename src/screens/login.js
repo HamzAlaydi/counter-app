d58095a3b40210b88611a0af97c2901ab31b3b59
Hamza Alaydi <hamza.alaydi.99@gmail.com>
2022-01-24 00:29:17 +0200

    third Commet

--- src/screens/login.js
+++ src/screens/login.js
@@ -4,6 +4,10 @@ const login = () => {
   return (
     <div>
       <h1>By the name of Allah</h1>
+      <p>1) git cheackout -b "branch name"</p>
+      <p>2) git push</p>
+      <p>3) copy text and past it</p>
+      <p>4)push and enjoy</p>
     </div>
   );
 };
